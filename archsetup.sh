26db6316a44e11cdd62f64311da90abbe4ce2830
David Farias <114120661+Kentsino@users.noreply.github.com>
2022-10-11 14:49:50 -0300

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+# Script made by Kenzino https://github.com/Kentsino
 
-#Updating the system
+# Updating the system
 echo "Syncing repos and updating packages, dont forget to check archlinux.org before updating/installing packages after syncing"
 sudo pacman -Syu
 
-#Installing programs
+# Installing programs
 echo "Installing essentials"
 sudo pacman -S firefox neovim ufw apparmor nvidia amd-ucode cuda htop zsh flameshot xorg libx11 libxinerama libxft dunst nitrogen wget lf
 
-#Creating directories 
+# Creating directories 
 echo "Creating home directories"
 mkdir git wallpapers suckless aur
 
-#Installing suckless programs
+# Installing suckless programs
 echo "Installing suckless"
 cd suckless/ ; wget https://dl.suckless.org/dwm/dwm-6.4.tar.gz && tar -xzvf dwm-6.4.tar.gz
 wget https://dl.suckless.org/tools/dmenu-5.2.tar.gz && tar -xzvf dmenu-5.2.tar.gz
 wget https://dl.suckless.org/st/st-0.9.tar.gz && tar -xzvf st-0.9.tar.gz
 rm dwm-6.4.tar.gz dmenu-5.2.tar.gz st-0.9.tar.gz
 
-#Compiling suckless programs
+# Compiling suckless programs
 echo "Compiling suckless"
 cd ~/suckless/dwm-6.4/ ; make && sudo make install 
 cd ~/suckless/dmenu-5.2/ ; make && sudo make install 
