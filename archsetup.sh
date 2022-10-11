8d6a0ea5b5d5156fe5e1f59a155e1ff9285bed6a
david <114120661+Kentsino@users.noreply.github.com>
2022-10-11 15:06:24 -0300

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -7,7 +7,7 @@ sudo pacman -Syu
 
 # Installing programs
 echo "Installing essentials"
-sudo pacman -S firefox neovim ufw apparmor nvidia amd-ucode cuda htop zsh flameshot xorg libx11 libxinerama libxft dunst nitrogen wget lf feh
+sudo pacman -S firefox neovim ufw apparmor nvidia amd-ucode cuda htop zsh flameshot xorg libx11 libxinerama libxft dunst nitrogen wget lf feh github-cli
 
 # Creating directories 
 echo "Creating home directories"
