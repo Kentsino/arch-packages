8a3265a134047ed4237195889dc6dad1ad3452f9
David Farias <114120661+Kentsino@users.noreply.github.com>
2022-10-11 14:28:23 -0300

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -8,9 +8,13 @@ sudo pacman -Syu
 echo "Installing essentials"
 sudo pacman -S firefox neovim ufw apparmor nvidia amd-ucode cuda htop zsh flameshot xorg libx11 libxinerama libxft dunst nitrogen wget
 
+#Creating directories 
+echo "Creating home directories"
+mkdir git wallpapers suckless aur
+
 #Installing suckless programs
 echo "Installing suckless"
-mkdir suckless ; cd suckless/ ; wget https://dl.suckless.org/dwm/dwm-6.4.tar.gz && tar -xzvf dwm-6.4.tar.gz
+cd suckless/ ; wget https://dl.suckless.org/dwm/dwm-6.4.tar.gz && tar -xzvf dwm-6.4.tar.gz
 wget https://dl.suckless.org/tools/dmenu-5.2.tar.gz && tar -xzvf dmenu-5.2.tar.gz
 wget https://dl.suckless.org/st/st-0.9.tar.gz && tar -xzvf st-0.9.tar.gz
 rm dwm-6.4.tar.gz dmenu-5.2.tar.gz st-0.9.tar.gz
