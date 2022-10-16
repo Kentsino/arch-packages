9d951b08cef65f2616eeaad81386cc921651ecd3
david <114120661+Kentsino@users.noreply.github.com>
2022-10-16 15:58:40 -0300

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -7,7 +7,7 @@ sudo pacman -Syu
 
 # Installing programs
 echo "Installing essentials"
-sudo pacman -S firefox pacman-contrib kitty neovim ufw apparmor nvidia amd-ucode cuda htop pulseaudio pavucontrol xorg-xinit flameshot xorg libx11 libxinerama zsh zsh-autosuggestions zsh-syntax-highlighting libxft dunst nitrogen wget lf feh ttf-jetbrains-mono patch
+sudo pacman -S firefox pacman-contrib alacritty neovim ufw apparmor nvidia amd-ucode cuda htop pulseaudio pavucontrol xorg-xinit flameshot xorg libx11 libxinerama zsh zsh-autosuggestions zsh-syntax-highlighting libxft dunst nitrogen wget lf feh ttf-jetbrains-mono patch
 
 # Creating directories 
 echo "Creating home directories"
